f47a67e105cead3170db0a4fff4da2b23876f2ac
juampipescie <76536672+juanpescie@users.noreply.github.com>
2021-12-16 10:03:41 -0300

    pre y post de jugador

--- src/Jugador.h
+++ src/Jugador.h
@@ -13,12 +13,23 @@ private:
 	int cantidadDeCartasDisponibles;
 
 public:
+	// pre: atributos validos, y no vacios, ficha disitnto de espacio
+	// post: una instancia de jugador con los atributos que se le pasan por parametros, con un nombre, y un identificador de ficha para el tablero 
 	Jugador(std::string nombre, char ficha);
+	// pre: ninguna 
+	// post: devuelve el atributo nojmbre del jugador
 	std::string obtenerNombre();
-	int obtenerNumeroDeJugador();
+	// pre: ninguna
+	// post: devuelve la cantidad de juegos que gano ese juagdor
 	int obtenerCantidadDePartidasGanadas();
+	// pre ninguna
+	// post: suma 1 a la cantidad de cartas disponibles para el jugador 
 	void agregarCarta();
+	// pre: ninguna
+	// post: devuelve el caracter que tiene como identificador ese jugador
 	char obtenerFicha();
+	// pre: el jugador gano la gano la partida
+	// post: el jugador suma 1 en su atributo cantidadDePartidasGanadas
 	void ganoLaPartida();
 };
 
